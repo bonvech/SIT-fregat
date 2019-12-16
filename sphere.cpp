fbc2ba7aaf62e7956fe3d33fdffcaeb9ee9230cd
bonvech.dell <bonvech@yandex.ru>
2019-12-16 14:43:47 +0300

    sphere.cpp with functions

--- sphere.cpp
+++ sphere.cpp
@@ -3,9 +3,9 @@
  * \author ptrwww@mail.ru
  * \version 2.0
  * \brief Программа автономной работы установки СФЕРА в Тунке
- * \date  октябрь 2019 года
+ * \date  октябрь-ноябрь 2019 года
  *
- * Данный файл содержит в себе основную программу для автономной работы 
+ * Данный файл содержит в себе основную программу для автономной работы
  * установки СФЕРА-2 c SiPM в наземном варианте в Тункинской долине.
  *
  * Убраны модули GPS, компас и инклинометер. 49 измерительных каналов (8 плат).
@@ -22,78 +22,222 @@
 
 #include "include/include.h"
 
+
+int open_ports_and_files();
+int wait_start_time(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[]);
+int wait_enable(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[]);
+int prepare_for_measurements(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[]);
+void close_ports_and_files();
+
+
+/** ------------------------------------------------------------------------
+ *
+ *
+ */
 int main (void)
 {
-    int  res   = 0, ii = 0;
-    long delta = 0;
-    struct timeval tv0, tv1, tv0sec;
-    struct tm* ptm0;
-    time_t time0;
-    char info[200];
+    int  res   = 0;
+
+    // -------------------------------------
+    ///        1) Initialization
+    // -------------------------------------
+
+    open_ports_and_files();
+
+    /// \todo чтение файла пока не появится корректная дата начала и конца
+    res = read_input_files();
+    if(res < 0)
+    {
+        print_debug((char*)"Errors in reading input TIME file!");
+        close_ports_and_files();
+        return -1;
+    }
+
+
+    // -------------------------------------
+    ///         2) Declare and init devices  
+    // -------------------------------------
+
+    SiPM       vip;         // class vip, on  // init vip
+    fadc_board Fadc;        // class fadc on  // init and boot fadc
+    lvps_dev   Vent;        // class lvps - ventillator
+    trigger_board Trigger;  // class trigger // boot and init trigger
+    led       LED;          // LED calibration system
+    barometer Bar[NumBar];  // Two barometers
+
+    /// Fill array TriggerOnOff according to input file
+    Trigger.fill_TriggerOnOff( Work.trigger_onoff);
+    bars_init(LED, Bar);
+
+    Every_sec(Fadc, Trigger, Vent);
+    Every_min_mini( vip, Vent, LED, Bar);
+    if(dout) fflush(dout);
+
+
+    // -------------------------------------
+    ///        3.0) Wait ENABLE signal
+    // -------------------------------------
+
+READ_START_TIME:
+
+    //  Read start time from file
+    Work.timeOnOff = read_date_from_file();
+    if(Work.timeOnOff.time_on < 10)
+    {
+        print_debug((char*)"\n\nDATA PANIC!!!!!\nError in reading DATE information!!!\n Check file \"config/sdate.inp\"!");
+        goto EXIT;
+    }
+
+
+    // -------------------------------------
+    ///        3.1) Wait ENABLE signal or stop time
+    // -------------------------------------
+
+WAIT_ENABLE:
+
+    res = wait_enable(Fadc, vip, Trigger, Vent, LED, Bar);
+    if(res == 1) // exit command
+        goto EXIT;
+    if(res == 2) // Time off
+    {
+        Fadc.turn_off_fadc_boards();
+        goto READ_START_TIME;
+    }
+
+    // Enable to run or command start in command file
+    Fadc.boot();
+
+
+    // -------------------------------------
+    ///        3.2) Wait start time
+    // -------------------------------------
+
+    res = wait_start_time(Fadc, vip, Trigger, Vent, LED, Bar);
+    if(res == 1)  // Exit command
+        goto EXIT;
+
+
+    // -------------------------------------
+    ///         4) Start time
+    //  Set high, set levels
+    // -------------------------------------
+LEVELS:
+
+    res = prepare_for_measurements(Fadc, vip, Trigger, Vent, LED, Bar);
+    if(res == 1) // Exit command
+        goto EXIT;
+
+    // -------------------------------------
+    ///         5) Main run 
+    // -------------------------------------
+
+    // Main Operation loop
+    res = Operate(Fadc, vip, Trigger, Vent, LED, Bar);
+
+    if(res == 2) // levels
+        goto LEVELS;
+
+    vip.turn_off();
+    if(res == 3) // Disable
+        goto WAIT_ENABLE;
+    if(res == 0) // Time off
+    {
+        Fadc.turn_off_fadc_boards();
+        write_disable();
+        goto WAIT_ENABLE;
+    }
+
+
+    // -------------------------------------
+    // -------------------------------------
+    ///         6) Exit
+    // -------------------------------------
+EXIT:
+
+    sprintf(msc_out, "Status: Exit. Turn off VIP and FADC");
+    print_status_to_file();
+
+    ///     Turn off VIP and FADC
+    if( Work.off)
+    {
+        vip.turn_off();
+        Fadc.turn_off_fadc_boards();
+    }
+    sprintf(vip_out, "Mosaic is not available now");
+    sprintf(msc_out, "Status: Exit");
+    print_status_to_file();
+
+    print_debug((char*)"\ninn_vent off:\n");
+    Vent.set_inn_vent(0);
+    print_debug((char*)"out vent off:\n");
+    Vent.set_out_vent(0);
+
+    // -------------------------------------
+    ///         6) Close ports and files
+    // -------------------------------------
+    close_ports_and_files();
+
+    return res;
+}
+
+
+/* ----------------------------------------------- */
+/* ----------------------------------------------- */
+/* ----------------------------------------------- */
+/* ----------------------------------------------- */
+
+/** -----------------------------------------------
+ * Initialization procedures before programm start
+ */
+int open_ports_and_files()
+{
+    int  res = 0;
 
     Last.temp_top = 0;
     Last.temp_bot = 0;
     Last.high_inn = 0;
     Last.high_out = 0;
 
-    // -------------------------------------
-    /// 1) Initialization 
-    // -------------------------------------
     GetIOPortsAccess();
     open_debug_file();
-    print_time();
-
-    /// \todo чтение файла пока не появится корректная дата начала и конца
-    res = read_input_files();
-    if(res < 0)
-    {
-        printf("Errors in reading input TIME file!");
-        return -1;
-    }
-    //printf("Onscreen: %i", Work.onscreen);
 
     // -------------------------------------
-    /// Open files for online telemetry monitoring 
+    // Open files for online telemetry monitoring 
     res = open_telemetry_files();
     if(res < 0)
     {
-        sprintf(info, "Errors to open telemetry files %d!", res);
-        print_debug(info);
+        sprintf(debug, "\n\nErrors to open telemetry files >%d!\n\n", res);
+        print_debug(debug);
     }
 
     // -------------------------------------
-    /// Kill another programm that works with apparatus
+    // Kill another programm that works with apparatus
     if(search_competitor()) // if LOG_FILE is changing
     {
         kill_competitor();  // alpha:  kill process PROC_TO_KILL 
     }
 
-    // -------------------------------------
-    /// 2) Declare and init devices  
-    // -------------------------------------
-
-    // print info status
+    //  print info status
     sprintf(msc_out, "Status: Initialization");
     print_status_to_file();
 
-    SiPM  vip;              // class vip, on  // init vip
-    fadc_board Fadc ;       // class fadc on  // init and boot fadc
-    lvps_dev   Vent;        // class lvps - ventillator
-    trigger_board Trigger;  // class trigger // boot and init trigger
-    led  LED;               // LED calibration system
-    barometer Bar[3];       // Three barometers
+    return 0;
+}
 
-    /// Fill array TriggerOnOff according to input file
-    Trigger.fill_TriggerOnOff( Work.trigger_onoff); 
-    bars_init(LED, Bar);
 
-    Every_sec(Fadc, vip, Trigger, Vent);
-    Every_min_mini( Vent, LED, Bar);
-    if(dout) fflush(dout);
-
-    // -------------------------------------
-    /// 3) Wait start time
-    // -------------------------------------
+/** -----------------------------------------------
+ * Wait programm start
+ * \return 0 - time to start\n
+ *         1 - time to exit: exit command in command file
+ * 
+ */
+int wait_start_time(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[])
+{
+    struct tm* ptm0;
+    time_t time0;
+    char info[200] = "Info init string";
+    struct timeval tv1, tv0sec, tv0min;
+    int res = 0;
 
     // print start time to info status message
     ptm0 = localtime(&Work.timeOnOff.time_on);
@@ -102,9 +246,11 @@ int main (void)
     print_status_to_file();
 
     gettimeofday(&tv1,    NULL);
-    gettimeofday(&tv0sec, NULL);
+    tv0sec.tv_sec = tv1.tv_sec;
+    tv0min.tv_sec = tv1.tv_sec;
+
     time(&time0);
-    sprintf(info, "start= %ld, now= %ld time0= %ld\n", Work.timeOnOff.time_on, tv1.tv_sec, time0);
+    sprintf(info, "\nstart= %ld, now= %ld time0= %ld\n", Work.timeOnOff.time_on, tv1.tv_sec, time0);
     print_debug(info);
 
     while( (Work.wait) && (tv1.tv_sec < Work.timeOnOff.time_on))
@@ -113,14 +259,16 @@ int main (void)
         /// Every sec routine
         if((tv1.tv_sec - tv0sec.tv_sec) > 0)
         {
-            Every_sec(Fadc, vip, Trigger, Vent);
-            gettimeofday(&tv0sec, NULL);
+            Every_sec(Fadc, Trigger, Vent);
+            //gettimeofday(&tv0sec, NULL);
+            tv0sec.tv_sec = tv1.tv_sec;
         }
 
         /// Every minute:
-        if(!(sec5 % SEC60))
+        if((tv1.tv_sec-tv0min.tv_sec) > SEC60)
         {
-            Every_min_mini( Vent, LED, Bar);
+            Every_min_mini(vip, Vent, LED, Bar);
+            tv0min.tv_sec = tv1.tv_sec;
         }
 
         ///  Read command file
@@ -130,14 +278,15 @@ int main (void)
         {
             sprintf(info, "read_command_file: %d commands\n", res);
             print_debug(info);
-            for(ii = 1; ii <= res; ii++)
+            for(int ii = 1; ii <= res; ii++)
             {
                 strcpy(info,"");
                 if(commandin[ii] == 1) // exit
                 {
                     strcpy(info, "\ncommand EXIT in file\n");
                     print_debug(info);
-                    goto EXIT;
+                    //goto EXIT;
+                    return 1;
                 }
                 if(commandin[ii] == 3) //start
                 {
@@ -149,11 +298,104 @@ int main (void)
         }
         //  end of read command file
     }
+    return 0;
+}
 
-    // -------------------------------------
-    // -------------------------------------
-    ///         4) Start time 
-    // -------------------------------------
+
+/** -----------------------------------------------
+ * \brief Wait enable status\n
+ * Read enable status from enable file or command file to start now.
+ * \return 0 - Enable to run or command start in command file\n
+ *         1 - command to exit\n
+ *         2 - time if off
+ */
+int wait_enable(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[])
+{
+    int    res = 0;
+    char   info[200] = "Info init string";
+    struct tm*     ptm0;
+    struct timeval tv1, tv0sec, tv0min;
+
+    // print status to info status message
+    ptm0 = localtime(&Work.timeOnOff.time_of);
+    strftime(info, sizeof(info), "%Y-%m-%d %H:%M:%S", ptm0);
+    sprintf(msc_out, "Status: Waiting for ENABLE or Stop time: %s", info);
+    print_status_to_file();
+
+    gettimeofday(&tv1, NULL);
+    tv0sec.tv_sec = tv1.tv_sec;
+    tv0min.tv_sec = tv1.tv_sec;
+ 
+    while(tv1.tv_sec < Work.timeOnOff.time_of)
+    {
+        gettimeofday(&tv1, NULL);
+
+        ///  Read status file
+        res = read_enable();
+        // if ENABLE - exit
+        if(res == 1)
+            return 0;
+
+        /// Every sec routine
+        if((tv1.tv_sec - tv0sec.tv_sec) > 0)
+        {
+            Every_sec(Fadc, Trigger, Vent);
+            //gettimeofday(&tv0sec, NULL);
+            tv0sec.tv_sec = tv1.tv_sec;
+        }
+
+        /// Every minute:
+        //if(!(sec5 % SEC60))
+        if((tv1.tv_sec-tv0min.tv_sec) > SEC60)
+        {
+            Every_min_mini(vip, Vent, LED, Bar);
+            tv0min.tv_sec = tv1.tv_sec;
+        }
+
+        ///  Read command file
+        res = 0;
+        res = read_command_file();
+        if(res > 0)
+        {
+            sprintf(info, "read_command_file: %d commands\n", res);
+            print_debug(info);
+            for(int ii = 1; ii <= res; ii++)
+            {
+                strcpy(info,"");
+                if(commandin[ii] == 1) // exit
+                {
+                    strcpy(info, "\ncommand EXIT in file\n");
+                    print_debug(info);
+                    //goto EXIT;
+                    return 1;
+                }
+                if(commandin[ii] == 3) //start
+                {
+                    strcpy(info, "\ncommand START in file\n");
+                    print_debug(info);
+                    Work.wait = 0;
+                    return 0;
+                }
+                print_debug(info);
+            }
+        }
+        //  end of read command file
+    }
+    return 2; // time off
+}
+
+
+/** -----------------------------------------------
+ * Procedures before measurement start.
+ * Set high, set levels.
+ * \return 0 OK\n
+ *         1 no condition for high
+ */
+int prepare_for_measurements(fadc_board &Fadc, SiPM &vip, trigger_board &Trigger, lvps_dev &Vent, led &LED, barometer Bar[])
+{
+    //int  res = 0;
+    long delta = 0;
+    struct timeval tv0, tv1;
 
     sprintf(msc_out, "Status: Start");
     print_status_to_file();
@@ -166,34 +408,36 @@ int main (void)
     ///         Set high voltage 
     sprintf(msc_out, "Status: Set High voltage");
     print_status_to_file();
-
-#ifndef NO_SET_HIGH
     gettimeofday(&tv0, NULL);
+
 #ifndef NO_SCOUT
-    //res = vip.high_scout(1,1);  // probe if high voltage setting is possible
-    res = vip.high_scout(ADDR,SUBADDR);  // !!! 2018: SiPM: probe if high voltage setting is possible
-#endif
-    if(!res) vip.high();  // set high voltage
-    else
+    // !!! 2018: SiPM: probe if high voltage setting is possible
+    if( res = vip.high_scout(ADDR,SUBADDR) ) // errors
     {
-        sprintf(info, "No conditions for high on!!: res = %i\n", res);
-        print_debug(info);
-        goto EXIT;
+        sprintf(debug, "No conditions for high on!!: res = %i\n", res);
+        print_debug(debug);
+        //goto EXIT;
+        return 1;
     }
+#endif
+
+    // set high voltage
+    vip.high();  // set high voltage
+
     gettimeofday(&tv1, NULL);
     delta = tv1.tv_sec - tv0.tv_sec;
-    sprintf(info, "\n==high==> delta = %ld sec\n", delta);
-    print_debug(info);
+    sprintf(debug, "\n==high==> delta = %ld sec\n", delta);
+    print_debug(debug);
     if(dout) fflush(dout);
-#endif
+
+    Every_min_mini(vip, Vent, LED, Bar);
 
     // -------------------------------------
     ///          Set levels 
-LEVELS:
+
     sprintf(msc_out, "Status: Set levels");
     print_status_to_file();
 
-#ifndef NO_SET_LEVELS
     gettimeofday(&tv0, NULL);
     Fadc.levels();
     gettimeofday(&tv1, NULL);
@@ -201,7 +445,6 @@ LEVELS:
     printf("\n==levels==>delta = %ld sec\n", delta);
     if(dout) fprintf(dout, "\n==levels==> delta = %ld sec\n", delta);
     if(dout) fflush(dout);
-#endif
 
     // -------------------------------------
     ///          Trigger status
@@ -213,72 +456,31 @@ LEVELS:
     if(dout) fflush(dout);
     sleep(10);  // 10sec
     if(dout) fclose(dout);
-    printf("               closed\n"); // !!!! no print to debu in this place !!!
+    printf("               closed\n"); // !!!! no print to debug in this place !!!
+
     open_debug_file();
     print_debug((char*)"debug open! \n");
     if(dout) fflush(dout);
 
-
-    // -------------------------------------
-    // -------------------------------------
-    ///         5) Main run 
-    // -------------------------------------
-
-    // print stop time to message
-    ptm0 = localtime(&Work.timeOnOff.time_of);
-    strftime(info, sizeof(info), "%Y-%m-%d %H:%M:%S", ptm0);
-    sprintf(msc_out, "Status: Operation. Waiting for stop time: %s", info);
-    print_status_to_file();
+    return 0;
+}
 
 
-    // Main Operation loop
-    Every_min_mini( Vent, LED, Bar);
-    res = Operate(Fadc, vip, Trigger, Vent, LED, Bar);
-    if(res == 2) // levels
-        goto LEVELS;
-
-
-    // -------------------------------------
-    // -------------------------------------
-    ///         6) Exit
-    // -------------------------------------
-EXIT:
-
-    sprintf(msc_out, "Status: Exit. Turn off VIP and FADC");
-    print_status_to_file();
-
-#ifndef NO_OFF
-    ///     Turn off VIP and FADC
-    if( Work.off)
-    {
-        vip.turn_off();
-        Fadc.turn_off_fadc_boards();
-    }
-    sprintf(vip_out,"Mosaic is not available now");
-    sprintf(msc_out, "Status: Exit");
-    print_status_to_file();
-#endif
-
-    print_debug((char*)"\ninn_vent off:\n");
-    Vent.set_inn_vent(0);
-    print_debug((char*)"out vent off:\n");
-    Vent.set_out_vent(0);
-
-
-
-    /// Close files
-    if(dout)   fclose(dout);
-    if(stderr) fclose(stderr);
-    if(stdout) fclose(stdout);
-    if(fkadr)  fclose(fkadr);
-    if(ffmin)  fclose(ffmin);
-
-    sprintf(msc_out, "Status: Bye! I do not work now.");
-    print_status_to_file();
-    if(f5sec)  fclose(f5sec);
-
+/** -----------------------------------------------
+ * Finish procedures after programm finish
+ */
+void close_ports_and_files()
+{
     /// Close Ports
     CloseIOPortsAccess();
 
-    return res;
+    sprintf(msc_out, "Status: Bye! I do not work now.");
+    print_status_to_file();
+
+    /// Close files
+    if(fkadr)  fclose(fkadr);
+    if(f5sec)  fclose(f5sec);
+    if(ffmin)  fclose(ffmin);
+    if(dout)   fclose(dout);
+    //if(stderr) fclose(stderr);
 }
